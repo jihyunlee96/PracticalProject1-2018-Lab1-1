c78cd75ba2e2f2abc790f7e9c389b1aaef064fc6
jihyunlee <jihyunlee@172.17.220.240>
2018-10-02 21:48:04 +0900

    #7 Implemented testMod method

--- src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
+++ src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
@@ -28,7 +28,8 @@ public class CalculatorTest {
 	
 	@Test
 	public void testMod() {
-		// TODO Complete this test case
+		Calculator myCalc = new Calculator();
+		assertEquals(myCalc.mod(20, 7),6);
 	}
 
 	@Test
